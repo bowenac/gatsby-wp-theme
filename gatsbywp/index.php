089317150bb67f27b61a68c4c160900a97eda47f
Adam <adamcbowen81@hotmail.com>
2020-01-07 12:26:11 -0800

    Cleanup formatting, added example of redirecting WordPress front end to a Gatsby site.

--- gatsbywp/index.php
+++ gatsbywp/index.php
@@ -1,3 +1,18 @@
-<?php
-//Themes needs an index.php file, we don't want to render anything on the front end of our WordPress site though.
-echo "Nothing to see here!";
+<?php
+/*
+* Themes needs an index.php file to work.
+* We probably don't want to render anything on the front end of our WordPress site though.
+* However we could display a simple message if we wanted to, otherwise remove it.
+*/
+echo 'Nothing to see here!';
+
+/*
+* If we wanted to, we could redirect the WordPress front end to our gatsby site.
+* Just uncomment below replacing example.com with our gatsby url.
+*/
+
+/*
+wp_safe_redirect( $url );
+wp_redirect( 'https://example.com' );
+exit;
+*/
